3b2a3731933b981c102b7f37698ddcb80176b809
Tanvi <tanvirangray@gmail.com>
2026-09-19 01:33:01 +0530

    Understanding thread pool

---
+++ build/CMakeFiles/thread_pool.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/thread_pool.dir/src/ThreadPool.cpp.o"
+  "CMakeFiles/thread_pool.dir/src/ThreadPool.cpp.o.d"
+  "libthread_pool.a"
+  "libthread_pool.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/thread_pool.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/thread_pool_app.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/thread_pool_app.dir/src/main.cpp.o"
+  "CMakeFiles/thread_pool_app.dir/src/main.cpp.o.d"
+  "thread_pool_app"
+  "thread_pool_app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/thread_pool_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
